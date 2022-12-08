3703ee6ba87917b17c6181b2bec87e5c52c6e5d1
Max <maxvhhg@gmail.com>
2022-12-08 11:21:02 +0300

    22]

--- src/ru/mirea/lab21/CatalogContents.java
+++ src/ru/mirea/lab21/CatalogContents.java
@@ -5,10 +5,10 @@ import java.util.LinkedList;
 import java.util.List;
 
 public class CatalogContents {
-    public static List<File> list = new LinkedList<>();
+    public static List<Object> list = new LinkedList<>();
     public static void main(String[] args) {
         File file = new File("/home/vhhg/");
-        for(File f : file.listFiles()){
+        for(Object f : file.listFiles()){
             list.add(f);
         }
         for(int i=0;i<5;++i) {
